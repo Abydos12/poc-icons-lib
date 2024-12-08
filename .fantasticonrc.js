4f89d2524e82271c7c217f8027ea6000761397a9
Abydos12 <38505162+Abydos12@users.noreply.github.com>
2024-12-08 12:02:35 +0100

    improve

--- .fantasticonrc.js
+++ .fantasticonrc.js
@@ -1,23 +1,14 @@
+/** @type {import('@twbs/fantasticon').RunnerOptions} */
 module.exports = {
   inputDir: "icons",
   outputDir: "./dist",
   prefix: "icon",
-  fontTypes: ["woff2"],
-  assetTypes: ["ts", "css", "json", "html"],
-  formatOptions: {
-    woff: {
-      // Woff Extended Metadata Block - see https://www.w3.org/TR/WOFF/#Metadata
-    },
-    ts: {
-      // customise names used for the generated types and constants
-      enumName: "MyIconType",
-      constantName: "MY_CODEPOINTS",
-      // literalIdName: 'IconId',
-      // literalKeyName: 'IconKey'
-    },
-  }, // Use a custom Handlebars template
+  fontTypes: ["ttf", "woff", "woff2"],
+  assetTypes: ["ts", "scss", "css", "json", "html"],
+  normalize: true, // Use a custom Handlebars template
   templates: {
     css: "./templates/css.hbs",
+    html: "./templates/html.hbs",
   },
   pathOptions: {
     // ts: "./src/types/icon-types.ts",
